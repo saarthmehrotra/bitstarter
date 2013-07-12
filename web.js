894ac2b959537deb60105555430dee6e2d9bdee2
saarthmehrotra <saarth.mehrotra@gmail.com>
2013-07-12 04:33:01 +0000

    Trying again 2.0 add index try js code

--- web.js
+++ web.js
@@ -5,12 +5,18 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html));
+    var fileName = fs.readFileSync("index.html");
+    var buff = new Buffer(fileName);
+    for var i = 0; i < fileName.length; i++){
+    buff[i] = strcharCodeA(i);
+}
 
-
+response.send(buff);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
