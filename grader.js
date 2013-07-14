fb21ea70f4e6a8f0cae2e525d2e43cd6050ac0d9
saarthmehrotra <saarth.mehrotra@gmail.com>
2013-07-14 06:01:18 +0000

    Updated Grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var restler = require('restler')
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +66,19 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+        .option('-u, --url ', 'URL of the webgpage')
+        .parse(process.argv)
+
+ if (program.url) {
+        restler.get(program.url).on('complete', function(result) {
+            var checkJson = checkHtmlFile(result, program.checks);
+    });
+    } else if (program.file) {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
