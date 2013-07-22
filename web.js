67200672db1b8cf45eb2e4cef22b9a93db5bf1c9
Ubuntu <ubuntu@ip-172-31-17-55.us-west-2.compute.internal>
2013-07-22 14:15:11 +0000

    Added responsive feature using bootstrap

--- web.js
+++ web.js
@@ -4,12 +4,14 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
+var htmlfile = "index.html";
+
 app.get('/', function(request, response) {
-    var fileName = fs.readFileSync("index.html");
-    response.send(fileName.toString('utf-8'));
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
